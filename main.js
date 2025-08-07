6b83feafdcea4020c85668f0e363a2578bc84da5
Caleb <calebmcgee06@gmail.com>
2025-08-06 22:08:45 -0400

    Complete form fields and add more validation

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
 const form = document.querySelector("form");;
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#email + span")
-// const country = document.querySelector("#country");
-// const postal = document.querySelector("#postal");
-// const password = document.querySelector("#password");
-// const passwordConfirm = document.querySelector("#passwordConfirm");
+const country = document.querySelector("#country");
+const countryError = document.querySelector("#country + span.error");
+const postal = document.querySelector("#postal");
+const postalError = document.querySelector("#postal + span.error");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#password + span.error");
+const passwordConfirm = document.querySelector("#passwordConfirm");
+const passwordConfirmError = document.querySelector("#passwordConfirm + span.error");
 
 email.addEventListener("input", (event)=>{
     if (!email.validity.valid){
         showEmailError();
         event.preventDefault();
+    } else {
+        emailError.textContent = "";
     }
 });
 
+country.addEventListener("input", (event)=>{
+    if(!country.validity.valid){
+        showCountryError();
+        event.preventDefault();
+    } else {
+        countryError.textContent = "";
+    }
+});
+
+postal.addEventListener("input", (event)=>{
+    if(!postal.validity.valid){
+        
+        showPostalError();
+        event.preventDefault();
+    } else {
+        postalError.textContent = "";
+    }
+});
+
+password.addEventListener("input", (event)=>{
+    if(!password.validity.valid){
+        showPasswordError();
+        event.preventDefault();
+    } else {
+        passwordError.textContent = "";
+    }
+});
+passwordConfirm.addEventListener("input", (event)=>{
+    if(!passwordConfirm.validity.valid){
+        showPasswordConfirmError();
+        event.preventDefault();
+        
+    } else if(password.value != passwordConfirm.value) {
+        passwordConfirm.style.backgroundColor = "rgb(255, 123, 123)";
+        passwordConfirmError.textContent = "Password does not match";
+    } else {
+        passwordConfirm.style.backgroundColor = "inherit";
+        passwordConfirmError.textContent = "";
+    }
+});
 
 const showEmailError = () => {
     if (email.validity.valueMissing){
@@ -20,4 +66,34 @@ const showEmailError = () => {
     } else if (email.validity.typeMismatch){
         emailError.textContent = "Incorrect format. Please enter valid email address."
     }
+}   
+const showCountryError = () =>{
+    if (country.validity.valueMissing){
+        countryError.textContent = "Enter a country";
+    } 
 }
+
+const showPostalError = () =>{
+    if (postal.validity.valueMissing){
+        postalError.textContent = "Enter postal code.";
+    } else if (postal.validity.typeMismatch){
+        postalError.textContent = "Please enter a number.";
+    } else if (postal.validity.rangeUnderflow || postal.validity.rangeOverflow){
+        postalError.textContent = "Postal code must be 5 digits.";
+    }
+} 
+
+const showPasswordError = () =>{
+    if (password.validity.valueMissing){
+        passwordError.textContent = "Enter password.";
+    } else if (password.validity.tooShort){
+        passwordError.textContent = "Password must be at least 8 characters.";
+    }
+} 
+
+const showPasswordConfirmError = () =>{
+    if (passwordConfirm.validity.valueMissing){
+        passwordConfirmError.textContent = "Enter password again.";
+    }
+}
+
